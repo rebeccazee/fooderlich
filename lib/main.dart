374ebbe1786fbb9e5866567f0a8c8502f55887c4
Rebecca Zee <ryz283@gmail.com>
2022-06-20 01:55:25 -0400

    3.3 Setting a theme 3.3.3 Using the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'fooderlich_theme.dart';
+
 void main() {
   // 1
   runApp(const Fooderlich());
@@ -10,18 +12,24 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // 3
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       // 4
       home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        body: Center(
+          child: Text('Let\'s get cooking 👩‍🍳',
+              style: theme.textTheme.headline1),
+        ),
       ),
     );
   }
