1de94dc8196be361443737b01626e0b89aad0aa7
Rebecca Zee <ryz283@gmail.com>
2022-06-21 18:17:58 -0400

    Chapter 5 starter files

--- lib/components/author_card.dart
+++ lib/components/author_card.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'fooderlich_theme.dart';
 
+import '../fooderlich_theme.dart';
 import 'circle_image.dart';
 
 class AuthorCard extends StatefulWidget {
-  // 1
   final String authorName;
   final String title;
   final ImageProvider? imageProvider;
@@ -17,13 +16,12 @@ class AuthorCard extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AuthorCard> createState() => _AuthorCardState();
+  _AuthorCardState createState() => _AuthorCardState();
 }
 
 class _AuthorCardState extends State<AuthorCard> {
   bool _isFavorited = false;
 
-  // 2
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -54,18 +52,15 @@ class _AuthorCardState extends State<AuthorCard> {
             ],
           ),
           IconButton(
-            // 1
             icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
             iconSize: 30,
-            // 2
             color: Colors.red[400],
             onPressed: () {
-              // 3
               setState(() {
                 _isFavorited = !_isFavorited;
               });
             },
-          )
+          ),
         ],
       ),
     );
