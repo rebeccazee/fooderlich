f55bfaa69542419be77722cbd497452bb8dc1a21
Rebecca Zee <ryz283@gmail.com>
2022-06-23 16:28:29 -0400

    6.4 Creating the empty Grocery screen 6.4.3 Adding the visual pieces

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -14,10 +14,37 @@ class EmptyGroceryScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // TODO 4: Add empty image
-            // TODO 5: Add empty screen title
-            // TODO 6: Add empty screen subtitle
-            // TODO 7: Add browse recipes button
+            // 1
+            Flexible(
+              // 2
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
+            ),
+
+            Text(
+              'No Groceries',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+
+            const SizedBox(height: 16.0),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+              textColor: Colors.white,
+              child: const Text('Browse Recipes'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                // TODO 8: Go to Recipes Tab
+              },
+            ),
           ],
         ),
       ),
