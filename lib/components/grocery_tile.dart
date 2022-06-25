4e1cad12e879da7ac0f9657d3be17cfdd4b9975b
Rebecca Zee <ryz283@gmail.com>
2022-06-25 02:32:08 -0400

    6.11 Building GroceryTile 6.11.2 Displaying the selected date

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -51,7 +51,14 @@ class GroceryTile extends StatelessWidget {
     }
   }
 
-  // TODO: Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
 
   // TODO: Add buildCheckbox()
 }
