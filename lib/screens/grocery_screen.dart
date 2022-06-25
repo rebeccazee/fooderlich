f481ac2b0040f6fc45214ec593cbacbfae6fbc06
Rebecca Zee <ryz283@gmail.com>
2022-06-25 02:57:21 -0400

    6.14 Creating GroceryListScreen 6.14.1 Adding items to the Grocery screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -51,8 +52,7 @@ class GroceryScreen extends StatelessWidget {
       builder: (context, manager, child) {
         // 3
         if (manager.groceryItems.isNotEmpty) {
-          // TODO 25: Add GroceryListScreen
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           // 4
           return const EmptyGroceryScreen();
