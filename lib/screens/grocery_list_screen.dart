adaefcfa10ef1778ae65d03c368aedcbefd8f68f
Rebecca Zee <ryz283@gmail.com>
2022-07-05 12:15:54 -0400

    6.15 Adding gestures 6.15.2 Adding an InkWell

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -25,17 +25,34 @@ class GroceryListScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           final item = groceryItems[index];
           // TODO 28: Wrap in a Dismissable
-          // TODO 27: Wrap in an InkWell
-          // 5
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
-            // 6
-            onComplete: (change) {
-              // 7
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+          return InkWell(
+            child: GroceryTile(
+                key: Key(item.id),
+                item: item,
+                onComplete: (change) {
+                  if (change != null) {
+                    manager.completeItem(index, change);
+                  }
+                }),
+            // 2
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GroceryItemScreen(
+                    originalItem: item,
+                    // 3
+                    onUpdate: (item) {
+                      // 4
+                      manager.updateItem(item, index);
+                      // 5
+                      Navigator.pop(context);
+                    },
+                    // 6
+                    onCreate: (item) {},
+                  ),
+                ),
+              );
             },
           );
         },
