73295a327bbc2e906c48fcbc20d5c4524fef1f79
Rebecca Zee <ryz283@gmail.com>
2022-07-05 11:37:48 -0400

    6.14 Creating GroceryListScreen 6.14.2 Creating a GroceryList view

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -13,7 +13,37 @@ class GroceryListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO 26: Replace with ListView
-    return Container();
+    // 1
+    final groceryItems = manager.groceryItems;
+    // 2
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      // 3
+      child: ListView.separated(
+        // 4
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = groceryItems[index];
+          // TODO 28: Wrap in a Dismissable
+          // TODO 27: Wrap in an InkWell
+          // 5
+          return GroceryTile(
+            key: Key(item.id),
+            item: item,
+            // 6
+            onComplete: (change) {
+              // 7
+              if (change != null) {
+                manager.completeItem(index, change);
+              }
+            },
+          );
+        },
+        // 8
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16.0);
+        },
+      ),
+    );
   }
 }
